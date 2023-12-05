6c670ce73e028dc2936a91fca3a73e9a4f76a1af
h41pa <madalin_chelu@yahoo.com>
2023-12-05 15:52:13 +0200

    Merge remote-tracking branch 'origin/main'

--- demoqa_com/tests/test_Datepicker.py
+++ demoqa_com/tests/test_Datepicker.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.select import Select
 from webdriver_manager.chrome import ChromeDriverManager
 from demoqa_com.pages.basepage import BasePage
 
@@ -12,6 +13,12 @@ from demoqa_com.pages.basepage import BasePage
 class TestDatepicker(unittest.TestCase, BasePage):
     LINK = 'https://demoqa.com/date-picker'
     select_date_locator = (By.XPATH, '//*[@id="datePickerMonthYearInput"]')
+    day_select = (By.XPATH, '//div[@aria-label="Choose Tuesday, January 19th, 2010"]')
+    month_select = (By.XPATH, '//select[@class="react-datepicker__month-select"]')
+    year_select = (By.XPATH, '//select[@class="react-datepicker__year-select"]')
+
+    date_and_time_locator = (By.ID, 'dateAndTimePickerInput')
+    year_second_selec = (By.CLASS_NAME, 'react-datepicker__month-read-view--selected-month')
 
     def setUp(self):
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -32,4 +39,20 @@ class TestDatepicker(unittest.TestCase, BasePage):
         time.sleep(1)
 
     def test_select_date_using_select(self):
+        self.click(self.select_date_locator)
+        time.sleep(1)
+        Select(self.find(self.year_select)).select_by_value('2010')
+        Select(self.find(self.month_select)).select_by_value('0')
+        self.click(self.day_select)
+        self.assertEqual('01/19/2010', self.find(self.select_date_locator).get_attribute('value'))
+        time.sleep(2)
+
+    def test_date_and_time_with_send_keys(self):
+        self.find(self.date_and_time_locator).send_keys(Keys.CONTROL, 'a')
+        time.sleep(1)
+        self.type(self.date_and_time_locator, 'December 1, 2023 8:00 AM')
+        time.sleep(2)
+        self.assertEqual('December 1, 2023 8:00 AM', self.find(self.date_and_time_locator).get_attribute('value'),
+                         'Not Expected Response !')
+        print(self.find(self.date_and_time_locator).get_attribute('value'))
 
